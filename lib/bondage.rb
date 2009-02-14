a8c8dfe56dc9ea51c4a670c7437172bcd3aae8d8
Justin Love <git@JustinLove.name>
2009-02-14 12:00:01 -0600

    gem wasn't picking up description from separate file

--- lib/bondage.rb
+++ lib/bondage.rb
@@ -1,13 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-=begin rdoc
-:include:description.rdoc
-
-== SYNOPSIS:
-
-  :include:synopsis.rb
-=end
+#:include:README.rdoc
 
 module Bondage
   VERSION = '0.0.1'
